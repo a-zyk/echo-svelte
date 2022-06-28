6afb99fd46038453b48011f215d509f354047743
Austėja Zykutė <austejazykute@gmail.com>
2022-06-28 16:20:14 +0300

    Updates

---
+++ _app/immutable/pages/index.svelte-0d6109c1.js
@@ -0,0 +1,9 @@
+import{S as b,i as x,s as A,e as l,t as j,k as P,c as u,a as c,h as y,d as n,m as E,b as v,g as D,G as i,E as h}from"../chunks/index-f3157f53.js";function I(_){let a,t,e,d,m,s,k;return{c(){a=l("div"),t=l("div"),e=l("p"),d=j(`Labas! \u0160is projektas yra skirtas sukurti echoskopijos apra\u0161ymo pagrind\u0105. Tikiuosi tai pad\u0117s
+		pagerinti echoskopijos apra\u0161ymo techinik\u0105 bei pagreitins veterinaro darb\u0105. Sugeneruot\u0105 tekst\u0105 galite kopijuoti ar toliau
+		naudoti/redaguoti kaip jums patinka. Projekta k\u016Briau, kaip mokymosi priemon\u0119.`),m=P(),s=l("p"),k=j(`Informacijos
+		\u0161altinis: Atlas of small animal ultrasonography 2nd edition by Dominique Penninck, Marc-Andr\xE9
+		d\u2019Anjou.`),this.h()},l(o){a=u(o,"DIV",{class:!0});var r=c(a);t=u(r,"DIV",{class:!0});var p=c(t);e=u(p,"P",{});var f=c(e);d=y(f,`Labas! \u0160is projektas yra skirtas sukurti echoskopijos apra\u0161ymo pagrind\u0105. Tikiuosi tai pad\u0117s
+		pagerinti echoskopijos apra\u0161ymo techinik\u0105 bei pagreitins veterinaro darb\u0105. Sugeneruot\u0105 tekst\u0105 galite kopijuoti ar toliau
+		naudoti/redaguoti kaip jums patinka. Projekta k\u016Briau, kaip mokymosi priemon\u0119.`),f.forEach(n),m=E(p),s=u(p,"P",{});var g=c(s);k=y(g,`Informacijos
+		\u0161altinis: Atlas of small animal ultrasonography 2nd edition by Dominique Penninck, Marc-Andr\xE9
+		d\u2019Anjou.`),g.forEach(n),p.forEach(n),r.forEach(n),this.h()},h(){v(t,"class","card max-w-lg"),v(a,"class","w-full flex items-center justify-center mt-36")},m(o,r){D(o,a,r),i(a,t),i(t,e),i(e,d),i(t,m),i(t,s),i(s,k)},p:h,i:h,o:h,d(o){o&&n(a)}}}class q extends b{constructor(a){super(),x(this,a,null,I,A,{})}}export{q as default};
